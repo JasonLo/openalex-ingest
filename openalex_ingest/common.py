daff4dbb4acb978fd9c8f20643d7000af5a8c3db
Jason Lo <lcmjlo@gmail.com>
2024-11-16 23:56:19 +0000

    add context manager to handle db session

--- openalex_ingest/common.py
+++ openalex_ingest/common.py
@@ -1,36 +1,53 @@
+import logging
 import os
+from contextlib import contextmanager
 from urllib.parse import urlparse
 
-import logging
 import sqlalchemy
+from dotenv import load_dotenv
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-S3_BUCKET = 'openalex-ingest'
+load_dotenv()
+S3_BUCKET = os.getenv("S3_BUCKET", "openalex-ingest")
+
 
 def _make_logger():
-    logger = logging.getLogger('openalex-ingest')
+    logger = logging.getLogger("openalex-ingest")
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
+
 
 LOGGER = _make_logger()
 
 
 def get_database_url():
     """Convert postgres:// to postgresql:// if necessary"""
-    database_url = os.getenv('DATABASE_URL')
+    database_url = os.getenv("DATABASE_URL")
     if not database_url:
         raise ValueError("DATABASE_URL environment variable is not set")
 
     parsed = urlparse(database_url)
-    if parsed.scheme == 'postgres':
-        return database_url.replace('postgres:', 'postgresql:', 1)
+    if parsed.scheme == "postgres":
+        return database_url.replace("postgres:", "postgresql:", 1)
     return database_url
+
 
 engine = sqlalchemy.create_engine(get_database_url())
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
-db = Session()
+
+
+@contextmanager
+def get_db():
+    """Resource management for database session."""
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
